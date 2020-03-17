9a4d5718847f9217f2d4cd6dfa4236a68d8bd05d
Vasileios Mitrousis <vamitrou@gmail.com>
2020-03-17 13:51:44 +0100

    changed send_mail

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	//"bufio"
-	"bytes"
+	//"bytes"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -10,16 +10,18 @@ import (
 	"net"
 	"net/smtp"
 	//"strconv"
-	"strings"
 	"github.com/BurntSushi/toml"
+	"strings"
 	"time"
 )
 
 type Config struct {
 	Ports       []int
 	SMTP_Server string
+	SMTP_User   string
+	SMTP_Passwd string
 	Mail_From   string
-	Mail_To     string
+	Mail_To     []string
 	Timeout     int64
 }
 
@@ -27,29 +29,26 @@ var conf Config
 var conn_cache = make(map[string]int64)
 
 func send_mail(ip string, port string) bool {
-        if conf.SMTP_Server == "" || conf.Mail_From == "" || conf.Mail_To == "" {
-            fmt.Printf("SMTP settings not configured.. \n")
-            return false
-        }
-	c, err := smtp.Dial(conf.SMTP_Server)
+	if conf.SMTP_Server == "" || conf.Mail_From == "" || len(conf.Mail_To) == 0 {
+		fmt.Printf("SMTP settings not configured.. \n")
+		return false
+	}
+	var auth smtp.Auth
+	if conf.SMTP_User != "" && conf.SMTP_Passwd != "" {
+		hostname := strings.Split(conf.SMTP_Server, ":")[0]
+		auth = smtp.PlainAuth("", conf.SMTP_User, conf.SMTP_Passwd, hostname)
+	}
+
+	msg := []byte(fmt.Sprintf("Subject: Network intrusion alert\r\n\r\n"+
+		"Connection attempt from %s at port %s\n", ip, port))
+
+	err := smtp.SendMail(conf.SMTP_Server, auth, conf.Mail_From, conf.Mail_To, msg)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Printf("Error sending email. \n")
+		return false
 	}
-	defer c.Close()
-	// Set the sender and recipient.
-	c.Mail(conf.Mail_From)
-	c.Rcpt(conf.Mail_To)
-	// Send the email body.
-	wc, err := c.Data()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer wc.Close()
-	buf := bytes.NewBufferString(fmt.Sprintf("Connection attempt from %s at port %s\n", ip, port))
-	if _, err = buf.WriteTo(wc); err != nil {
-		log.Fatal(err)
-	}
-        return true
+
+	return true
 }
 
 func report(ip string, port string) {
@@ -59,10 +58,10 @@ func report(ip string, port string) {
 	key := fmt.Sprintf("%s%s", ip, port)
 	v, found := conn_cache[key]
 	if !found || v+conf.Timeout < now {
-            sent := send_mail(ip, port)
+		sent := send_mail(ip, port)
 		if sent {
-                    fmt.Printf("Mail sent. \n")
-                }
+			fmt.Printf("Mail sent. \n")
+		}
 		conn_cache[key] = now
 	}
 }
@@ -119,7 +118,7 @@ func load_config(path string) {
 }
 
 func main() {
-        load_config("config.toml")
+	load_config("config.toml")
 	for i := 0; i < len(conf.Ports); i++ {
 		PORT := fmt.Sprintf(":%d", conf.Ports[i])
 		go serve(PORT)
